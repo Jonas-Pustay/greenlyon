311392cb16502b29a25210c0d869c3c5f6b4a450
Jonas-Pustay <95695354+Jonas-Pustay@users.noreply.github.com>
2024-12-10 13:13:56 +0000

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+function openApp() {
+    alert("L'application sera bientôt disponible !");
+    // Vous pouvez rediriger l'utilisateur vers l'application ou une page de téléchargement
+    // window.location.href = 'https://votre-application.com';
+}
